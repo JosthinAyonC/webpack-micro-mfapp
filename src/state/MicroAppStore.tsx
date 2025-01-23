ed2197b3e7a3f71d12580b7cadb916e383adcfca
Chustin <josthinayon@gmail.com>
2025-01-23 14:28:00 -0500

    feat: update commons

--- src/state/MicroAppStore.tsx
+++ src/state/MicroAppStore.tsx
@@ -1,9 +1,7 @@
-import authSlice from './slices/authSlice';
 import userSlice from './slices/userDataSlice';
 
 const microappslices = {
   // Aquí se van a ir agregando los slices personalizados de la aplicación.
-  auth: authSlice,
   user: userSlice,
 };
 
