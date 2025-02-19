8881c29e876238c7eeef115451dbe1161eb75d36
Chustin <josthinayon@gmail.com>
2025-02-19 09:26:47 -0500

    feat: fix dockerfile

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,8 @@ module.exports = (_, argv) => {
 
   return {
     output: {
-      publicPath: `/`,
+      // TODO: IMPROVE THIS, WE NEED CALCULATE THE PUBLIC PATH BASED ON THE CURRENT DOMAIN
+      publicPath: isProduction ? `${process.env.CURRENT_DOMAIN}/` : `http://localhost:${process.env.APP_PORT}/`,
     },
 
     resolve: {
